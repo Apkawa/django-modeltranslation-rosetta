6a81d000777c8d17e687d8dfb34cb07aff82e142
apkawa <apkawa@gmail.com>
2017-02-15 13:26:39 +0300

    Update utils

--- modeltranslation_rosetta/management/commands/_utils.py
+++ modeltranslation_rosetta/management/commands/_utils.py
@@ -1,9 +1,16 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+from django.db.models import Model
+
+
+def get_opts_from_model(model):
+    fields = [(f.name, None) for f in model._meta.get_fields()]
+    return dict(app_label=model._meta.app_label, model_name=model._meta.model_name, fields=fields)
+
 
 def parse_model(model):
-    return dict(zip(['app_label', 'model', 'field'], model.split('.') + [None] * 2))
+    return dict(zip(['app_label', 'model', 'field'], model.lower().split('.') + [None] * 2))
 
 
 def has_exclude(model_opts, excludes):
@@ -16,9 +23,13 @@ def has_exclude(model_opts, excludes):
     if not excludes:
         return False
 
+    if issubclass(model_opts, Model) or isinstance(model_opts, Model):
+        model_opts = get_opts_from_model(model_opts)
+
     app_label = model_opts['app_label']
     model_name = model_opts['model_name']
     fields = list(model_opts['fields'])
+
     _exclude = False
     for i in excludes:
         _exclude |= i['app_label'] == app_label and i['model'] is None
@@ -37,6 +48,9 @@ def has_exclude(model_opts, excludes):
 def has_include(model_opts, includes):
     if not includes:
         return True
+
+    if issubclass(model_opts, Model) or isinstance(model_opts, Model):
+        model_opts = get_opts_from_model(model_opts)
 
     app_label = model_opts['app_label']
     model_name = model_opts['model_name']
